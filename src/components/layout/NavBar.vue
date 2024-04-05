b1d39973264bf1d9a4741c0de46907d0fe1bbb9b
Leeyah-123 <junaidaaliyah260@gmail.com>
2024-04-05 12:55:17 +0100

    feat: UI changes

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -6,7 +6,7 @@ const isOpen = ref(false);
 
 <template>
   <nav
-    class="fixed inset-0 bottom-auto grid grid-cols-2 md:flex justify-between items-center px-7 py-5 bg-black bg-opacity-10 backdrop-blur-md mix-blend-hard-light text-white">
+    class="fixed inset-0 bottom-auto grid grid-cols-2 md:flex justify-between items-center px-7 py-5 backdrop-blur-md bg-black bg-opacity-10 mix-blend-hard-light text-white">
     <a href="/">
       <p class="sr-only">Back to Home</p>
       <img src="@/assets/logo.png" alt="AAN logo" class="italic" width="100" height="100" />
@@ -39,3 +39,11 @@ const isOpen = ref(false);
     </ul>
   </nav>
 </template>
+
+<style scoped>
+@-moz-document url-prefix() {
+  nav {
+    --tw-bg-opacity: 0.5 !important;
+  }
+}
+</style>
--- src/components/sections/ReviewsSection.vue
+++ src/components/sections/ReviewsSection.vue
@@ -4,7 +4,7 @@
     <img src="@/assets/white-robot.png" class="xl:w-full mt-auto xl:min-h-[40em]" alt="" />
 
     <div
-      class="w-full h-full flex flex-col self-center gap-6 lg:gap-14 xl:text-right xl:place-items-end xl:max-w-[50%]">
+      class="w-full h-full flex flex-col self-center gap-6 pb-10 lg:gap-14 xl:text-right xl:place-items-end xl:max-w-[50%]">
       <h1 class="text-[min(4rem,_10vw)] xl:text-8xl xl:tracking-tighter font-bold lg:max-w-[10ch]">Collect
         Reviews
         from Your Existing
