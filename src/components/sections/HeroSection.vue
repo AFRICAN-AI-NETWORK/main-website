4d9ec54d765b681fd9edd7f9ea8f444029a21f7d
Leeyah-123 <junaidaaliyah260@gmail.com>
2024-04-05 12:02:56 +0100

    feat: Fix typography

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -1,10 +1,11 @@
 <template>
   <section id="hero"
     class="flex flex-col justify-between lg:flex-row gap-x-5 px-14 pt-32 lg:pt-16 lg:px-20 pb-0 min-h-dvh bg-cover bg-[url('/src/assets/hero-bg.jpg')] text-white">
-    <div class="w-full h-full flex flex-col self-center gap-14 xl:max-w-[50%]">
-      <h1 class="text-7xl xl:text-8xl tracking-tighter font-bold max-w-[10ch]">Design. Think. Solve. and
+    <div class="w-full h-full flex flex-col self-center gap-6 lg:gap-14 xl:max-w-[50%]">
+      <h1 class="text-[min(4rem,_10vw)] xl:text-8xl tracking-tighter font-bold max-w-[10ch]">Design. Think. Solve. and
         Develop.</h1>
-      <p class="text-3xl xl:text-4xl">A collaborative hub for African AI Actors to explore, develop and implement AI
+      <p class="text-[min(2rem,_4vw)] xl:text-4xl">A collaborative hub for African AI Actors to explore, develop and
+        implement AI
         solutions.</p>
 
       <div class="flex flex-wrap gap-5">
--- src/components/sections/ReviewsSection.vue
+++ src/components/sections/ReviewsSection.vue
@@ -3,10 +3,12 @@
     class="flex flex-col-reverse justify-between lg:flex-row gap-x-5 p-16 pb-0 lg:px-20 min-h-dvh bg-secondary text-white">
     <img src="@/assets/white-robot.png" class="xl:w-full mt-auto xl:min-h-[40em]" alt="" />
 
-    <div class="w-full h-full flex flex-col self-center xl:text-right xl:place-items-end gap-14 xl:max-w-[50%]">
-      <h1 class="text-7xl xl:text-8xl tracking-tighter font-bold max-w-[10ch]">Collect Reviews from Your Existing
+    <div
+      class="w-full h-full flex flex-col self-center gap-6 lg:gap-14 xl:text-right xl:place-items-end xl:max-w-[50%]">
+      <h1 class="text-[min(4rem,_10vw)] xl:text-8xl tracking-tighter font-bold max-w-[10ch]">Collect Reviews
+        from Your Existing
         Clients</h1>
-      <p class="text-3xl xl:text-4xl">Feel free to drop your reviews on how we can serve you better.</p>
+      <p class="text-[min(2rem,_4vw)] xl:text-4xl">Feel free to drop your reviews on how we can serve you better.</p>
 
       <a class="btn w-fit px-8" href="mailto:africanainetwork@gmail.com">Review</a>
     </div>
