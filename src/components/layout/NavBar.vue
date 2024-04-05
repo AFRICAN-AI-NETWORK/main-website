32e45a338a5effae037e2360969e4e9d7cbc9287
Leeyah-123 <junaidaaliyah260@gmail.com>
2024-04-05 02:07:45 +0100

    feat: Make navbar responsive

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,10 +1,30 @@
-<template>
-  <nav class="fixed inset-0 bottom-auto flex justify-between items-center px-7 py-3 bg-primary text-white">
-    <img src="@/assets/logo.png" alt="AAN logo" class="italic" width="50" height="50" />
+<script setup lang="ts">
+import { ref } from 'vue';
 
-    <ul class="flex gap-5">
+const isOpen = ref(true);
+</script>
+
+<template>
+  <nav
+    class="fixed inset-0 bottom-auto grid grid-cols-2 md:flex justify-between items-center px-7 py-     3 bg-primary text-white">
+    <a href="/">
+      <p class="sr-only">Back to Home</p>
+      <img src="@/assets/logo.png" alt="AAN logo" class="italic" width="50" height="50" />
+    </a>
+
+    <button type="button"
+      class="justify-self-end md:hidden text-white hover:text-slate-100 focus:text-slate-100 focus:outline-none"
+      @click="isOpen = !isOpen">
+      <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
+        <path
+          d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
+        </path>
+      </svg>
+    </button>
+
+    <ul class="flex flex-col mt-3 md:flex-row md:mt-0 gap-5" :class="isOpen ? 'flex' : 'hidden md:flex'">
       <li>
-        <a href="#ai-tools" class="hover:text-secondary focus-visible:text-secondary transition-all">AI Tools</a>
+        <a href=" #ai-tools" class="hover:text-secondary focus-visible:text-secondary transition-all">AI Tools</a>
       </li>
       <li>
         <a href="#resources" class="hover:text-secondary focus-visible:text-secondary transition-all">Resources</a>
