0c3c518d6e3530d10cd0ea76fd7101e7b30c7836
Leeyah-123 <junaidaaliyah260@gmail.com>
2024-04-05 12:07:16 +0100

    feat: Fix typography

--- src/components/sections/ReviewsSection.vue
+++ src/components/sections/ReviewsSection.vue
@@ -5,7 +5,8 @@
 
     <div
       class="w-full h-full flex flex-col self-center gap-6 lg:gap-14 xl:text-right xl:place-items-end xl:max-w-[50%]">
-      <h1 class="text-[min(4rem,_10vw)] xl:text-8xl tracking-tighter font-bold max-w-[10ch]">Collect Reviews
+      <h1 class="text-[min(4rem,_10vw)] xl:text-8xl xl:tracking-tighter font-bold lg:max-w-[10ch]">Collect
+        Reviews
         from Your Existing
         Clients</h1>
       <p class="text-[min(2rem,_4vw)] xl:text-4xl">Feel free to drop your reviews on how we can serve you better.</p>
