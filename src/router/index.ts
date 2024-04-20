5afa73f416c84ce3d94cb66b5c4f50f3dbc437e5
Leeyah-123 <junaidaaliyah260@gmail.com>
2024-04-20 20:34:59 +0100

    feat: Add 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ResourceView from '../views/ResourceView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: HomeView
     },
     {
       path: '/ai-tools',
-      name: 'ai-tools',
+      name: 'AiTools',
       component: () => import('../views/AIToolsView.vue')
     },
     {
       path: '/resources/:slug',
-      name: 'resource',
-      component: ResourceView
+      name: 'Resource',
+      component: () => import('../views/ResourceView.vue')
+    },
+    {
+      path: '/ai-tools/:slug',
+      name: 'AiTool',
+      component: () => import('../views/AIToolView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('../views/NotFoundView.vue')
     }
   ]
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-dvh grid content-center text-center" v-if="!loading && !resource">
+    <h2 class="text-6xl font-bold pb-5">Page not found</h2>
+    <button class="btn text-xl w-fit mx-auto" @click="$router.replace('/')">Go back to
+      home</button>
+  </div>
+</template>
